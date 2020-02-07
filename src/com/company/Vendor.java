a394490e6d194e8b8fc5cafaceefd2c7c5dc29e2
brandon <brandonmarion@billericak12.com>
2020-02-07 10:57:15 -0500

    snackky

--- src/com/company/Vendor.java
+++ src/com/company/Vendor.java
@@ -1,65 +1,66 @@
 package com.company;
 
-public class Vendor
-{
+public class Vendor {
     // Fields:
     private int price;
     private int stock;
-  private int deposit;
-  private int change;
+    private int deposit;
+    private int change;
+    private static double totalSales;
 
 
-
-          /**
-           * Constructs a Vendor
-           * @param price the price of a single item in cents (int)
-           * @param stock number of items to place in stock (int)
-           */
-          public Vendor(int a , int b)
-          {
-            price = a;
-            stock = b;
-            deposit = 0;
-            change = 0;
+    /**
+     * Constructs a Vendor
+     *
+     * @param price the price of a single item in cents (int)
+     * @param stock number of items to place in stock (int)
+     */
+    public Vendor(int a, int b) {
+        price = a;
+        stock = b;
+        deposit = 0;
+        change = 0;
+        totalSales = 0;
 
     }
 
     /**
      * Sets the quantity of items in stock.
+     *
      * @param qty number of items to place in stock (int)
      */
-    public void setStock(int numberOfItems){
-      stock = numberOfItems;
+    public void setStock(int numberOfItems) {
+        stock = numberOfItems;
 
     }
 
     /**
      * Returns the number of items currently in stock.
+     *
      * @return number of items currently in stock (int)
      */
-    public int getStock()
-    {
-      return stock;
+    public int getStock() {
+        return stock;
     }
 
     /**
      * Adds a specified amount (in cents) to the
      * deposited amount.
+     *
      * @param number of cents to add to the deposit (int)
      */
-    public void addMoney(int cents)
-    {
-    deposit = cents + deposit;
+    public void addMoney(int cents) {
+        deposit = cents + deposit;
 
     }
 
     /**
      * Returns the currently deposited amount (in cents).
+     *
      * @return number of cents in the current deposit (int)
      */
-    public int getDeposit()
-  {
-   return deposit;
+    public int getDeposit() {
+        return deposit;
     }
 
     /**
@@ -69,29 +70,39 @@ public class Vendor
      * and calculates and sets change and returns true;
      * otherwise refunds the whole deposit (moves it into
      * change) and returns false.
+     *
      * @return true for a successful sale, false otherwise (boolean)
      */
-  public boolean makeSale() {
-      if (stock > 0 && deposit >= price) {
-          stock = stock - 1;
-          change = deposit - price;
-          return true;
-      } else {
-          deposit = deposit + change;
-          return false;
+    public boolean makeSale() {
+        if (stock > 0 && deposit >= price) {
+            stock = stock - 1;
+            change = deposit - price;
+            totalSales = totalSales + price;
+            return true;
+        } else {
+            change = deposit;
+            deposit = 0;
+            return false;
 
-      }
-  }
+        }
+    }
 
     /**
      * Returns and zeroes out the amount of change (from
      * the last sale or refund).
+     *
      * @return number of cents in the current change (int)
      */
-    public int getChange()
-    {
-       int a = change;
-       a = 0;
-       return a;
+    public int getChange() {
+        int a = change;
+        change = 0;
+        return a;
+    }
+
+    public static double getTotalSales(){
+        double b = totalSales / 100;
+        totalSales = 0;
+        return b;
+
     }
 }
