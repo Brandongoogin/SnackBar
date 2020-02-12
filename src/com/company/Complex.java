d262076a0e36947a7f95916cb40e9e9044dbdb32
Brandon <brandonmarion@billericak12.com>
2020-02-11 19:39:25 -0500

    yee

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -19,7 +19,27 @@ public class Complex {
         double ans = Math.sqrt(first * first + second * second);
         return ans;
     }
-    public Complex add(Complex other) {
-        double
+    public double add(Complex other) {
+        double ans = (first + other.first) + (second + other.second);
+        return ans;
+
     }
+    public String toString(){
+        return (first + " + " + second + " i");
+
+    }
+    public Complex multiply(Complex other) {
+        return new Complex (((first * other.first) - (second * other.second)), ((first * other.second) + (second * other.first)));
+
+    }
+    public Complex add(double third){
+        return new Complex ((first + third), (second + third));
+
+    }
+    public Complex multiply(double third){
+        return new Complex (((first * third) - (second * third)), ((first * third) + (second * third)));
+    }
+
+
+
 }
