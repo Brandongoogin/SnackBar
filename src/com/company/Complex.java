8a3dfb41f512da8a60f135d153b2a7ae1a198bb4
Brandon <brandonmarion@billericak12.com>
2020-02-11 20:52:55 -0500

    yeeeettt

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -15,9 +15,9 @@ public class Complex {
 
     }
 
-    public double abs(double a, double b) {
-        double ans = Math.sqrt(first * first + second * second);
-        return ans;
+    public double abs() {
+        return Math.sqrt(first * first + second * second);
+
     }
     public double add(Complex other) {
         double ans = (first + other.first) + (second + other.second);
@@ -29,7 +29,7 @@ public class Complex {
 
     }
     public Complex multiply(Complex other) {
-        return new Complex (((first * other.first) - (second * other.second)), ((first * other.second) + (second * other.first)));
+        return new Complex (((first * other.first) + (second * other.second)), ((first * other.second) + (second * other.first)));
 
     }
     public Complex add(double third){
@@ -38,6 +38,24 @@ public class Complex {
     }
     public Complex multiply(double third){
         return new Complex (((first * third) - (second * third)), ((first * third) + (second * third)));
+    }
+    public static void main(String[] args){
+        Complex c1 = new Complex(6,9);
+        Complex c2 = new Complex(-2, 4);
+        Complex c3 = new Complex(7,-5);
+
+        System.out.println(c1);
+        System.out.println(c1.abs());
+        System.out.println(c1.add(c2));
+        System.out.println(c1.add(4.0));
+        System.out.println(c1.multiply(c2));
+        System.out.println(c1.multiply(6.0));
+        System.out.println();
+        System.out.println(c2);
+        System.out.println(c2.abs());
+        System.out.println(c2.add(c3));
+        System.out.println(c2.multiply(c3));
+        System.out.println();
     }
 
 
